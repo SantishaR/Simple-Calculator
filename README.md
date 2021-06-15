35f072887523cd90d759b78720aa17312ed79ea0
SantishaR <82038785+SantishaR@users.noreply.github.com>
2021-06-15 20:03:57 +0200

    Added more functions

--- README.md
+++ README.md
@@ -3,9 +3,12 @@
 In this project we created a simple calculator.
 
 ## Languages
+
 HTML and JavaScript
 
 ## Credits
-Rorisang Kgwete
-Santish Reddy
+
+- Rorisang Kgwete
+- Santisha Reddy
+
 ## License
--- calculator.js
+++ calculator.js
@@ -1,4 +1,19 @@
+function add(num1,num2){
+    let total= num1 + num2;
+    console.log(total);
+}add(105,24);
+
+function subtract(num1,num2){
+    let total= num1 - num2;
+    console.log(total);
+}subtract(586,123);
+
 function multiply(num1,num2){
     let total= num1 * num2;
     console.log(total);
 }multiply(12,2);
+
+function divide(num1,num2){
+    let total= num1 / num2;
+    console.log(total);
+}divide(24,6);
